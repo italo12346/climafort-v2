c027ba10b280a7c53adbf4ca36d9a5e73ccf8c8f
Italo souza sampaio <italo12346@gmail.com>
2025-09-18 09:20:12 -0300

    fix: correção de rotas para deploy

--- src/app/api/pdfs/[file]/route.ts
+++ src/app/api/pdfs/[file]/route.ts
@@ -4,15 +4,14 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-interface Params {
-  file: string;
-}
-
-export async function GET(req: Request, context: { params: Params }) {
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ file: string }> }
+) {
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.redirect("/login");
 
-  const { file } = context.params;
+  const { file } = await params; // ✅ precisa do await
   const filePath = path.join(process.cwd(), "private_pdfs", file);
 
   if (!fs.existsSync(filePath)) {
